cfc1f92b0ba3d1cfc11bc53a3fc42fcaa4b43254
Chief <54397128+brianmwenda@users.noreply.github.com>
2020-05-19 12:14:11 +0300

    Update guitar.js

--- guitar.js
+++ guitar.js
@@ -3,7 +3,7 @@ const modelParams = {
   imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
   maxNumBoxes: 1,        // maximum number of boxes to detect
   iouThreshold: 0.5,      // ioU threshold for non-max suppression
-  scoreThreshold: 0.89,    // confidence threshold for predictions.
+  scoreThreshold: 0.99,    // confidence threshold for predictions.
 }
 //load webcam
 navigator.getUserMedia = navigator.getUserMedia ||
@@ -22,7 +22,7 @@ handTrack.startVideo(video).then(status => {
     navigator.getUserMedia({video:{}},stream => {
       video.srcObject = stream;
       //run detection
-      setInterval(runDetection,100);
+      setInterval(runDetection,80);
     },
     err => console.log(err)
   );
